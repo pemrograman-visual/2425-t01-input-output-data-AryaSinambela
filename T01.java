7e518ca0515d205d9b69e9b99e40be55e41698cb
AryaSinambela <aryasinambela26@gmail.com>
2024-09-06 11:12:47 +0700

    Kami telah menyelesaikan tugas T01

--- T01.java
+++ T01.java
@@ -1,1 +1,32 @@
-// NIM - Your Name
+// 12S24017 Arya Sinambela
+// 12S24057 Laura Sirumapea
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01{
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String isbn, judul, penulis;
+        int tahunterbit;
+        String penerbit;
+        String formatbuku;
+        double hargabeli, minimummargin;
+        int stok;
+        double rating;
+
+        isbn = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatbuku = input.nextLine();
+        hargabeli = input.nextDouble();
+        minimummargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(isbn + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargabeli + "|" + minimummargin + "|" + stok + "|" + rating);
+    }
+}
+
